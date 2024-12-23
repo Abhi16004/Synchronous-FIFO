f2b4490876f163702904ebbec73ca76de69fa602
Abhi16004 <abhirammd004@gmail.com>
2024-12-23 09:25:40 +0530

    Update FIFO_tb.v

--- FIFO_tb.v
+++ FIFO_tb.v
@@ -3,8 +3,10 @@ module tb_FIFO;
     parameter data_size = 8;
     parameter depth = 8;
 
-    // Testbench signals
-    reg clk, rst, writeEN, readEN;
+    reg clk;
+    reg rst;
+    reg writeEN;
+    reg readEN;
     reg [data_size-1:0] DIN;
     wire [data_size-1:0] DOUT;
     wire empty, full;
@@ -26,13 +28,12 @@ module tb_FIFO;
     initial clk = 0;
     always  #5 clk = ~clk; 
 
-    // Test sequence
     initial begin
-        // Initialize signals
+        //Reset FIFO.
         rst = 1; writeEN = 0; readEN = 0; DIN = 0;
         
-        // Reset the FIFO
         #8 rst = 0; writeEN = 1;
+        
         //Write data to FIFO.
         for (i=0; i<depth; i=i+1) begin 
             DIN <= $random % 256;
@@ -40,13 +41,14 @@ module tb_FIFO;
         end 
         
         writeEN = 0; readEN = 1;
+        
         //Read data from FIFO
         for (i=0; i<depth; i=i+1) begin
             #10;
         end
 
-        readEN = 0;                            //FIFO is empty now.
-        
+        readEN = 0;                           
+        //FIFO is empty now.
         // Finish simulation
         #20 $stop;
     end
